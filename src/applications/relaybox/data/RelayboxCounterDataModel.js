e8f96fbfed5c674cc424c858b18933eb55e4c1e0
Benoit Bacot <benoit.bacot@relaymark.com>
2016-09-09 17:37:59 +0200

    fixing building

--- src/applications/relaybox/data/RelayboxCounterDataModel.js
+++ src/applications/relaybox/data/RelayboxCounterDataModel.js
@@ -3,9 +3,6 @@ export default function RelayboxCounterDataModel($modelFactory) {
   var result = {
     forDataSourceSidAndCounter: function forDataSourceSid(dataSourceSid, counterName) {
       return $modelFactory('applications/relaybox/counter-data/' + dataSourceSid + '/' + counterName);
-    },
-    liveForDataSourceSidAndCounter: function (dataSourceSid, counterName) {
-      return $modelFactory('applications/relaybox/counter-data/' + dataSourceSid + '/' + counterName + '/live');
     }
   }; 
   return result;
