c4c1345a2cda14f0ec114f35cc3d053091efe518
Benoit Bacot <benoit.bacot@relaymark.com>
2016-09-28 15:59:48 +0200

    fixe naming

--- src/relaymark.model.relayboxDriverModel.js
+++ src/relaymark.model.relayboxDriverModel.js
@@ -3,8 +3,12 @@
  */
 import 'ng-file-upload';
 import './relayboxApiModelFactory';
-import CmxNetworkModel from './Relaybox-driver-api/CmxNetworkModel.js';
+import RbDriverCmxNetworkModel from './Relaybox-driver-api/RbDriverCmxNetworkModel';
+import RbDriverRegistrationModel from './Relaybox-driver-api/RbDriverRegistrationModel';
+import RbDriverEquipmentModel from './Relaybox-driver-api/RbDriverEquipmentModel';
 
 export default angular.module('relaymark.model.relayboxDriver', ['relayboxApiModelFactory'])
-  .factory('CmxNetworkModel', CmxNetworkModel)
+  .factory('RbDriverCmxNetworkModel', RbDriverCmxNetworkModel)
+  .factory('RbDriverRegistrationModel', RbDriverRegistrationModel)
+  .factory('RbDriverEquipmentModel', RbDriverEquipmentModel)
   .name;
