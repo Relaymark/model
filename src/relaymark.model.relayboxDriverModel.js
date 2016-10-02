c310115c9480f097386890d9c0098c71b35e0295
Benoit Bacot <benoit.bacot@relaymark.com>
2016-10-02 19:17:31 +0200

    wip

--- src/relaymark.model.relayboxDriverModel.js
+++ src/relaymark.model.relayboxDriverModel.js
@@ -7,8 +7,14 @@ import RbDriverCmxNetworkModel from './Relaybox-driver-api/RbDriverCmxNetworkMod
 import RbDriverRegistrationModel from './Relaybox-driver-api/RbDriverRegistrationModel';
 import RbDriverEquipmentModel from './Relaybox-driver-api/RbDriverEquipmentModel';
 
+import RbDriverInstallationOptionsModel from './Relaybox-driver-api/RbDriverInstallationOptionsModel';
+import RbDriverAccessPointModel from './Relaybox-driver-api/RbDriverAccessPointModel';
+
 export default angular.module('relaymark.model.relayboxDriver', ['relayboxApiModelFactory'])
   .factory('RbDriverCmxNetworkModel', RbDriverCmxNetworkModel)
   .factory('RbDriverRegistrationModel', RbDriverRegistrationModel)
   .factory('RbDriverEquipmentModel', RbDriverEquipmentModel)
+
+  .factory('RbDriverInstallationOptionsModel', RbDriverInstallationOptionsModel)
+  .factory('RbDriverAccessPointModel', RbDriverAccessPointModel)
   .name;
