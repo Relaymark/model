d22e4eb8953ccc25d2a7cb133434b8ad073abca9
Benoit Bacot <benoit.bacot@relaymark.com>
2016-10-02 19:20:33 +0200

    wip

--- dist/relaymark.model.relayboxDriverModel.js
+++ dist/relaymark.model.relayboxDriverModel.js
@@ -20,8 +20,16 @@ var _RbDriverEquipmentModel = require('./Relaybox-driver-api/RbDriverEquipmentMo
 
 var _RbDriverEquipmentModel2 = _interopRequireDefault(_RbDriverEquipmentModel);
 
+var _RbDriverInstallationOptionsModel = require('./Relaybox-driver-api/RbDriverInstallationOptionsModel');
+
+var _RbDriverInstallationOptionsModel2 = _interopRequireDefault(_RbDriverInstallationOptionsModel);
+
+var _RbDriverAccessPointModel = require('./Relaybox-driver-api/RbDriverAccessPointModel');
+
+var _RbDriverAccessPointModel2 = _interopRequireDefault(_RbDriverAccessPointModel);
+
 function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
 
-exports.default = angular.module('relaymark.model.relayboxDriver', ['relayboxApiModelFactory']).factory('RbDriverCmxNetworkModel', _RbDriverCmxNetworkModel2.default).factory('RbDriverRegistrationModel', _RbDriverRegistrationModel2.default).factory('RbDriverEquipmentModel', _RbDriverEquipmentModel2.default).name; /**
-                                                                                                                                                                                                                                                                                                                           * Created by benoit.bacot on 29/08/2016.
-                                                                                                                                                                                                                                                                                                                           */
+exports.default = angular.module('relaymark.model.relayboxDriver', ['relayboxApiModelFactory']).factory('RbDriverCmxNetworkModel', _RbDriverCmxNetworkModel2.default).factory('RbDriverRegistrationModel', _RbDriverRegistrationModel2.default).factory('RbDriverEquipmentModel', _RbDriverEquipmentModel2.default).factory('RbDriverInstallationOptionsModel', _RbDriverInstallationOptionsModel2.default).factory('RbDriverAccessPointModel', _RbDriverAccessPointModel2.default).name; /**
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           * Created by benoit.bacot on 29/08/2016.
+                                                                                                                                                                                                                                                                                                                                                                                                                                                                                           */
