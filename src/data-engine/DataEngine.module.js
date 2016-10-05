ed859358e359774b83fcf97f17b04e47f8757523
Denis Martinho <denis.martinho@fairefaire.fr>
2016-10-05 16:26:00 +0200

    Adding route for Customers

--- src/data-engine/DataEngine.module.js
+++ src/data-engine/DataEngine.module.js
@@ -2,6 +2,7 @@
 import DataSourceRegistrationModel from './DataSourceRegistrationModel.js';
 import DataEngineCounterModel from './DataEngineCounterModel.js';
 import DataEngineEventModel from './DataEngineEventModel.js';
+import DataEngineCustomerModel from './DataEngineCustomerModel.js';
 
 
 
@@ -9,4 +10,5 @@ export default angular.module('relaymark.model.data-engine', ['modelFactory'])
     .factory('DataSourceRegistrationModel', DataSourceRegistrationModel)
     .factory('DataEngineCounterModel', DataEngineCounterModel)
     .factory('DataEngineEventModel', DataEngineEventModel)
+    .factory('DataEngineCustomerModel', DataEngineCustomerModel)
     .name;
