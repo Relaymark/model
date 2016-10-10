2805a8f9dd21e4c551fb72f64224ef75aba4fd32
Denis Martinho <denis.martinho@fairefaire.fr>
2016-10-10 19:17:22 +0200

    maj routes data chunk

--- dist/data-engine/DataEngineDataChunkModel.js
+++ dist/data-engine/DataEngineDataChunkModel.js
@@ -6,10 +6,5 @@ Object.defineProperty(exports, "__esModule", {
 exports.default = DataEngineDataChunkModel;
 DataEngineDataChunkModel.$inject = ['$modelFactory'];
 function DataEngineDataChunkModel($modelFactory) {
-  var result = {
-    forDataSourceSIdAndDataChunkTypeName: function forDataSource(dataSourceSId, dataChunkTypeName) {
-      return $modelFactory('data-engine/data-chunks/' + dataSourceSId + '/' + dataChunkTypeName);
-    }
-  };
-  return result;
+  return $modelFactory('data-engine/data-chunks');
 }
--- src/data-engine/DataEngineDataChunkModel.js
+++ src/data-engine/DataEngineDataChunkModel.js
@@ -1,9 +1,4 @@
 DataEngineDataChunkModel.$inject = ['$modelFactory'];
-export default function DataEngineDataChunkModel($modelFactory) {
-  var result = {
-    forDataSourceSIdAndDataChunkTypeName: function forDataSource(dataSourceSId, dataChunkTypeName) {
-      return $modelFactory('data-engine/data-chunks/' + dataSourceSId + '/' + dataChunkTypeName);
-    }
-  };
-  return result;
+export default function DataEngineDataChunkModel($modelFactory) {    
+  return $modelFactory('data-engine/data-chunks');
 } 
